fbd2f7d52705c69a990a0232fc12aa0e4c5e712a
David Yim <yimd85@gmail.com>
2017-10-22 10:32:04 -0400

    update2serverfile

--- server.js
+++ server.js
@@ -2,25 +2,25 @@
 var express = require('express')
 var path    = require("path");
 var bodyParser= require('body-parser')
-var Stuff = require("./models/models.js");
+// var Stuff = require("./models/models.js");
 var app = express();
 
 const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
+// 
+// client.connect();
+//
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 
 app.set('view engine','pug');
