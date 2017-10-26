ac77be789bdad7a4554fec08748fb0e787f37caf
Martin Pham <martin.liem.pham@gmail.com>
2017-10-26 11:52:52 -0400

    minor changes pushing to pull code from david's multer changes

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,8 @@ var posts = require('./models/post');
 var multer = require('multer'); //require multer(coipied and pasted)
 
 var app = express()
-var upload = multer({ dest: 'uploads/' })
-var sharp = require('sharp')
+var upload = multer({ dest: './postImages/uploads' })
+// var sharp = require('sharp')
 
 
 
@@ -33,25 +33,47 @@ var myStorage = multer.diskStorage({
     cb(null, file.fieldname + '-' + Date.now() + '.' + file.mimetype.split('/')[1])
 } });
 
-
-//routes/products.js
+//MULTER DOCUMENTATION TEST
 var requestHandler = multer({ storage: myStorage });
 
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+	// req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
+})
+app.post('/photos/upload', upload.array('photos', 12), function (req, res, next) {
+	 // req.files is array of `photos` files 
+  // req.body will contain the text fields, if there were any 
+})
 
-router.post('./post', requestHandler.single('New Post'), function(req, res, next) {
+var cpUpload = upload.fields([{ name: 'avatar', maxCount: 1 }, { name: 'gallery', maxCount: 8 }])
+app.post('/cool-profile', cpUpload, function (req, res, next) {
+  // req.files is an object (String -> Array) where fieldname is the key, and the value is array of files 
+  // 
+  // e.g. 
+  //  req.files['avatar'][0] -> File 
+  //  req.files['gallery'] -> Array 
+  // 
+  // req.body will contain the text fields, if there were any 
+})
 
-		//call the sharp() function passing in the image we want to resize
-		sharp(__dirname + '/../postImages/uploads/' + req.file.filename)
-		// ignoreAspectRatio will not crop the image to fit the desired size
-		.ignoreAspectRatio()
-		// resize image to these dimensions (w x h) in pixels
-		.resize(200, 200)
-		// specify where to save the result
-		.toFile(__dirname + '/../postImages/uploads/' + req.file.filename, function(err, info) {
-			products.addProduct(req.body, req.file.filename)
-    res.redirect('./post');
-		});
-});
+
+
+
+//routes/products.js
+// router.post('./post', requestHandler.single('New Post'), function(req, res, next) {
+
+// 		//call the sharp() function passing in the image we want to resize
+// 		sharp(__dirname + '/../postImages/uploads/' + req.file.filename)
+// 		// ignoreAspectRatio will not crop the image to fit the desired size
+// 		.ignoreAspectRatio()
+// 		// resize image to these dimensions (w x h) in pixels
+// 		.resize(200, 200)
+// 		// specify where to save the result
+// 		.toFile(__dirname + '/../postImages/uploads/' + req.file.filename, function(err, info) {
+// 			products.addProduct(req.body, req.file.filename)
+//     res.redirect('./post');
+// 		});
+// });
 
 
 
