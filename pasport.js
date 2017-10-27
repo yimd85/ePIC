0dbc7b5352bf47560df7617af4d54fc0caf263ed
Martin Pham <martin.liem.pham@gmail.com>
2017-10-26 20:52:13 -0400

    tried to start passport and added a footer to the images not yet but soon to come

---
+++ pasport.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var passport = require('passport')
+var session = require('express-session')
+var Sequelize = require('sequelize'),
+var sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/example')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = sequelize
