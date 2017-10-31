72d0fbd1b083b64c9ad1a0a7e1bc702e490e7553
David Yim <yimd85@gmail.com>
2017-10-30 20:18:35 -0400

    added callback to post

--- routes/postsJS.js
+++ routes/postsJS.js
@@ -7,6 +7,26 @@ var path    = require("path");
 router.use(express.static(path.join(__dirname, 'photo')));
 
 
+var storage = multer.diskStorage({
+  destination: function(req, file, callback) {
+    callback(null, './photos')
+  },
+  filename: function(req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now() + '.' + file.mimetype.split('/')[1]);
+  }
+});
+
+var upload = multer({
+  storage: storage,
+  fileFilter: function(request, file, callback) {
+    var ext = path.extname(file.originalname)
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg'!== '.PNG' && ext !== '.JPG' && ext !== '.GIF' && ext !== '.JPEG') {
+      return callback(response.redirect('/go/home'), null)
+    }
+    callback(null, true)
+  }
+}).single('fileupload');
+
 // render homepage new stuff
 router.get('/home',function(request,response){
   Post.findAll().then(function(postArray){
@@ -20,52 +40,18 @@ router.get('/post',function(request,response){
 })
 
 
-
-
-
-
-var storage = multer.diskStorage({
-	destination: function(req, file, callback) {
-		callback(null, './photos')
-	},
-	filename: function(req, file, callback) {
-		callback(null, file.fieldname + '-' + Date.now() + '.' + file.mimetype.split('/')[1]);
-	}
-});
-
-
 router.post('/post', function(request, response) {
+  upload(request, response, function(err) {
+      var full = {
+        text: request.body.postmessage,
+        photoPath: `${"/photos/"}`+request.file.filename
+        }
 
-  var upload = multer({
-		storage: storage,
-		fileFilter: function(request, file, callback) {
-			var ext = path.extname(file.originalname)
-			if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-				return callback(response.end('Only images are allowed'), null)
-			}
-			callback(null, true)
-		}
-	}).single('fileupload');
-	upload(request, response, function(err) {
-
-    var fileUpload = "/photos/"+request.file.filename;
-    var posting = request.body.postmessage;
-    console.log(fileUpload);
-    console.log(posting);
-    Post.findAll().then(function(){
-      Post.create({
-        text: `${posting}`
-        ,
-        photoPath: `${fileUpload}`
-        })
-
-
-    // var newStuff = {text:posting, photoPath:fileUpload}
-    // postArray.push(newStuff);
-
-	   })
-  response.redirect('/go/home');
-  })
+      Post.create(full).then(
+        function(){
+          response.redirect('/go/home');
+  	   })
+    })
 });
 
 
--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/',function(request,response){
 
 
 var bioArray = [
-  {email: 'yimd85@gmail.com', firstN: 'david', lastN: 'yim',bioPath:''}
+  {email: 'yimd85@gmail.com', firstN: 'david', lastN: 'yim',bioPath:'../photos/sawsry.JPG'}
 ];
 
 //render profile
@@ -73,17 +73,19 @@ var storagePicPic = multer.diskStorage({
 	}
 });
 
-app.post('/go/profile',function(request,response){
-  var upload = multer({
-    storage: storagePicPic,
-    fileFilter: function(request, file, callback) {
-      var ext = path.extname(file.originalname)
-      if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-        return callback(res.end('Only images are allowed'), null)
-      }
-      callback(null, true)
+var upload = multer({
+  storage: storagePicPic,
+  fileFilter: function(request, file, callback) {
+    var ext = path.extname(file.originalname)
+    if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg'!== '.PNG' && ext !== '.JPG' && ext !== '.GIF' && ext !== '.JPEG') {
+      return callback(response.redirect('/go/profile'), null)
     }
-  }).single('uploadpictureForm');
+    callback(null, true)
+  }
+}).single('uploadpictureForm');
+
+
+app.post('/go/profile',function(request,response){
   upload(request, response, function(err) {
         var profilestuff = "../photos/profilepic/"+request.file.filename;
         bioArray[0].bioPath = profilestuff
@@ -91,47 +93,9 @@ app.post('/go/profile',function(request,response){
       })
 });
 
-//passport code
-// app.use(session({
-//   secret: "epic",
-//   store: new SequelizeStore({
-//     db: connection
-//   })
-// }));
-//
-// passport.use(new EpicStrategy(function(username, password, done) {
-//     console.log(username);
-//     console.log(password);
-//     User.findOne({where: {username: username}})
-//     .then(project =>{
-//       if(project === null){
-//         done(null,false);
-//       }else{
-//         console.log("username found in database");
-//           console.log(project.password)
-//           const hash = project.password
-//           const useridcurrent = project.id
-//           bcrypt.compare(password, hash, function(err, response){
-//             if(response === true){
-//               return done(null, {user_id: useridcurrent});
-//             }else{
-//               return done(null, false ,{message: "Incorrect Credentials"});
-//             }
-//           })
-//         }
-//
-//     })
-//
-//
-//     }));
-//     passport.serializeUser(function(user_name, done) {
-//       done(null, user_name);
-//     });
-//
-//     passport.deserializeUser(function(user_name, done) {
-//       done(null, user_name);
-//     });
-//end of passport code
+
+
+
 
 //catch all (delete this piece of code)
 app.get('*',function(request,response){
